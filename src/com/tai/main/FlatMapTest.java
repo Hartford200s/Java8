6683b907f2dbe9ed29a18df5d3a0e564df0761a3
tai <id18041804@gmail.com>
2016-07-05 23:42:47 +0800

    flatMaptest

--- src/com/tai/main/FlatMapTest.java
+++ src/com/tai/main/FlatMapTest.java
@@ -4,16 +4,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class FlatTest {
+public class FlatMapTest {
 	
 	public static void main(String args[]){
+		flatMapTest();
+	}
+	
+	public static void flatMapTest(){
 		Stream<List<Integer>> stream = Stream.of(
-					Arrays.asList(1,3,5),
-					Arrays.asList(2,4,6),
-					Arrays.asList(10,11)
-				);
-		
-		Stream<Integer> integerStrem = stream.flatMap(list -> list.stream());
-		integerStrem.forEach(System.out::println);
+				Arrays.asList(1,3,5),
+				Arrays.asList(2,4,6),
+				Arrays.asList(10,11)
+			);
+	
+	Stream<Integer> integerStrem = stream.flatMap(list -> list.stream());
+	integerStrem.forEach(System.out::println);
 	}
 }
